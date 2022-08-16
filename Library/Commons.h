ad7c1b32d682aab60dca3454653e82cc558a8874
Milos Vasic <milos85vasic@gmail.com>
2022-08-16 16:43:08 +0300

    Tokenize.

--- Library/Commons.h
+++ Library/Commons.h
@@ -5,10 +5,23 @@
 #ifndef COMMONS_COMMONS_H
 #define COMMONS_COMMONS_H
 
+#include <string>
+
 namespace Commons {
 
     namespace Strings {
 
+        void tokenize(std::string const &str, const char delim, std::list<std::string> &out) {
+
+            size_t start;
+            size_t end = 0;
+
+            while ((start = str.find_first_not_of(delim, end)) != std::string::npos) {
+
+                end = str.find(delim, start);
+                out.push_back(str.substr(start, end - start));
+            }
+        }
     }
 }
 
