06b0ab86859fd4920cfb7b2d14f5a6482942c41f
Milos Vasic <milos85vasic@gmail.com>
2022-08-24 18:19:15 +0300

    Tokenize vector.

--- Library/Commons.h
+++ Library/Commons.h
@@ -9,12 +9,25 @@
 #include <fstream>
 #include <iostream>
 #include <list>
+#include <vector>
 
 namespace Commons {
 
     namespace Strings {
 
         void tokenize(std::string const &str, const char delim, std::list<std::string> &out) {
+
+            size_t start;
+            size_t end = 0;
+
+            while ((start = str.find_first_not_of(delim, end)) != std::string::npos) {
+
+                end = str.find(delim, start);
+                out.push_back(str.substr(start, end - start));
+            }
+        }
+
+        void tokenize(std::string const &str, const char delim, std::vector<std::string> &out) {
 
             size_t start;
             size_t end = 0;
