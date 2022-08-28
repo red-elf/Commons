c38224413e376735be1e93ff8dd82ae44b9368fb
Milos Vasic <milos85vasic@gmail.com>
2022-08-28 15:14:03 +0300

    IRegistration.

---
+++ Library/IRegistration.h
@@ -0,0 +1,18 @@
+//
+// Created by milosvasic on 28.08.22.
+//
+
+#ifndef COMMONS_1_0_0_IREGISTRATION_H
+#define COMMONS_1_0_0_IREGISTRATION_H
+
+template<class T>
+class IRegistration {
+
+public:
+
+    virtual bool doRegister(T &what) = 0;
+    virtual bool doUnregister(T &what) = 0;
+};
+
+
+#endif //COMMONS_1_0_0_IREGISTRATION_H
